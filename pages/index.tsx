ffd4b641ff6d565a433e2711c39e722ab3cdd1e5
Glauber Magalhães <glauber.tecinfo@gmail.com>
2021-03-18 23:01:07 -0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      
+      HTS
+    </Layout>
+  )
+}
